c914163d55a2ec3405eeb72a25623b621bd1eb0b
Vitalii <vitaliikalyta96@gmail.com>
2021-12-15 22:17:03 +0200

    Events Edit Done.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Events from './Components/Events';
 import Certificates from './Components/Certificates';
 import SignIn from './Components/SignIn';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
+import EventsFunctional from "./Components/EventsEdit";
 
 function App() {
   return (
     <div className="App">
     <Events />
+    <EventsFunctional />
     <Certificates />
     <SignIn />
     </div>
