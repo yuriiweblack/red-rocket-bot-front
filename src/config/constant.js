221e7730ae6b170578c578edae356712a8b83b8e
Vitalii Kalyta <86627796+VitaliiKalyta96@users.noreply.github.com>
2022-02-06 21:21:41 +0200

    Update constant.js

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,3 +1,3 @@
 export const API_URL = "http://localhost:7072/api/v1/events";
-export const API2_URL = "http://localhost:7072/api/v1/certificate";
+export const API2_URL = "http://localhost:7072/api/v1/certificates";
 export const API3_URL = "http://localhost:7072/api/v1/events/categories";
